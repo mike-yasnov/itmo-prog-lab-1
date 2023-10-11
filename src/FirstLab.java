32d87c9ad9e63ae8b959d5ce9c5ace4125296449
Yasnov Mikhail <mike_yasnov@mail.ru>
2023-10-11 15:11:07 +0300

    Update

--- src/FirstLab.java
+++ src/FirstLab.java
@@ -1,24 +1,20 @@
-import java.util.Arrays;
-import java.util.Random;
 import java.lang.Math;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class FirstLab {
     public static void main(String[] args) {
         long[] c = new long[10];
         int k = 4;
+
         for (int i = 0; i < c.length; i++) {
             c[i] = k;
             k += 2;
         }
 
         double[] x = new double[14];
-        Random r = new Random();
         double min = -12.0, max = 8.0;
 
         for (int i = 0; i < x.length; i++) {
-            x[i] = min + (max - min) * r.nextDouble();
+            x[i] = min + (max - min) * Math.random();
         }
 
         double[][] array = new double[10][14];
@@ -30,7 +26,7 @@ public class FirstLab {
                                     Math.PI * (5 + Math.abs(x[j])), x[j])
                             )
                     );
-                } else if (Arrays.asList(8, 10, 14, 18, 22).contains(c[i])) {
+                } else if (c[i] == 8 || c[i] == 10 || c[i] == 14 || c[i] == 18 || c[i] == 22) {
                     array[i][j] = Math.tan(
                             Math.tan(
                                     Math.pow(x[j], (x[j] - 0.5) / x[j])
@@ -50,7 +46,7 @@ public class FirstLab {
 
         for (int i = 0; i < c.length; i++) {
             for (int j = 0; j < x.length; j++) {
-                System.out.printf("%.4f\t", array[i][j]);
+                System.out.printf("%15.4f ", array[i][j]);
             }
             System.out.println();
         }
